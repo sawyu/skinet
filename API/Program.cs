0b468b4b2b3c121fa6cb585d55fee33924e2b601
sawyu <sawyunwetun@gmail.com>
2022-03-03 09:41:09 +0630

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,32 @@
+using System;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using(var scope  = host.Services.CreateScope())
+            {
+                var servivces = scope.ServiceProvider;
+                var loggerFactory = servivces.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = servivces.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context,loggerFactory);
+                }
+                catch (Exception ex)
+                {
+                    
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex,"An error occured during migration");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
